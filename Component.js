30c15b2c0827759bdf275df4286aa48656ce377d
sandeepvh <129170342+sandeepvh@users.noreply.github.com>
2024-02-13 16:41:29 +0530

    Update Component.js

---
+++ Component.js
@@ -0,0 +1,30 @@
+// Display name in popup
+var myName = 'Sandeep';
+console.log(myName);
+console.log('Hi good morning')
+//using math, calculte 10+5 in the console
+  10+5
+// calculate 20-5 in the console
+   10+5
+//- You order a T-Shirt for Rs-10, socks for Rs-8, and dinner plates for Rs-20. Use JavaScript to calculate the total cost of your order.
+let tshirt=10;
+let socks=8;
+let dinnerPlates=20;
+
+let total = tshirt + socks + dinnerPlates;
+console.log(total);
+
+let accountBalance, lunch, dinner, salary;
+accountBalance=100;
+lunch=50;
+dinner=110;
+salary=500;
+
+let AvailableBalance = accountBalance - (lunch + dinner) + salary;
+console.log(AvailableBalance);
+
+
+
+
+
+
