1ba5735a1a9eddfccee7d9e935113043a31a2e0d
sandeepvh <129170342+sandeepvh@users.noreply.github.com>
2024-02-21 12:16:42 +0530

    Update Challenge1_SH.js

--- Challenge1_SH.js
+++ Challenge1_SH.js
@@ -1,3 +1,18 @@
+Challenge(1): [JavaScript Basics]
+Note: Do these exercises in the Console (right-click > Inspect > Console)
+- Use alert (....); to display "good morning!" in a popup.
+- Display your name in a popup.
+- Using Math, Calculate 10+5 in the Console.
+- Calculate 20-5 in the Console
+- Calculate 2+2-5 in the Console
+- Use document.body.innerHTML = ....; to display 'Good morning!' on the web page.
+- Display your name on the web page.
+- You order a T-Shirt for Rs-10, socks for Rs-8, and dinner plates for Rs-20. Use JavaScript to calculate the total cost of your order.
+- Your bank account has Rs-100, you spend Rs-20 on lunch, Rs-50 on dinner, and earn Rs-200 from your job. Calculate how much money you have.
+- Use document.body.innerHTML = ....; to make the web page blank.
+
+==============================================================================================================================
+  
 //- Use alert (....); to display "good morning!" in a popup.
 alert("good morning!");
 
