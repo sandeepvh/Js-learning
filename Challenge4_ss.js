d0ccd9ef260526951d218b7dc888dd0bf7fd53e0
sandeepvh <129170342+sandeepvh@users.noreply.github.com>
2024-02-21 11:56:31 +0530

    Create Challenge4_ss.js

---
+++ Challenge4_ss.js
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Js learning</title>
+</head>
+<body>
+    <button>Click</button> <br><br>
+
+    <button>Sandeep</button> <br> <br>
+
+    <button>Dosa</button> <br>
+
+    <p> "Hello World" </p>
+    <p> Today i am doing 4th Js challenge!</p>
+
+    <script>
+        console.log('Welcome');
+        
+        window.addEventListener('load', () => {
+          window.alert('Welcome');
+        });
+      </script>
+
+      <p> Adults Plain Cotton T-Shirt </p>
+      <p> Price: $7.99 </p>
+
+      <button> Add To Cart </button>
+      <button> Buy Now </button>
+
+    
+</body>
+</html>
